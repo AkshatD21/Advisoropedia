614eef93f57bc4a09f6c3eeb68011a8b5f11a318
Akshat <akshatd212003@gmail.com>
2024-02-21 01:35:33 +0530

    authentication done

---
+++ client/src/components/Signup.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import "../App.css";
+import Axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+
+const Signup = () => {
+  const [username, setUsername] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    Axios.post("http://localhost:3000/auth/signup", {
+      username,
+      email,
+      password,
+    })
+      .then((response) => {
+        if (response.data.status) {
+          navigate("/login");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log("kya  hora");
+      });
+  };
+
+  return (
+    <div className="sign-up-container">
+      <form className="sign-up-form" onSubmit={handleSubmit}>
+        <h2>Sign Up</h2>
+
+        <label htmlFor="username">Username: </label>
+        <input
+          type="text"
+          placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          autoComplete="off"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label htmlFor="password">Password: </label>
+        <input
+          type="password"
+          placeholder="******"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Sign Up</button>
+        <p>
+          Have an Account Already? <Link to="/login">Login</Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
