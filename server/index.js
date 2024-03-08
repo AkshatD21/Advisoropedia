8ea83f47f1a839febb32c1708a260cc236f8ff90
Akshat <akshatd212003@gmail.com>
2024-03-09 03:10:07 +0530

    adding vercel.json

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(cors({
     origin: ["http://localhost:5173"],
+    methods: ["POST", "GET"],
     credentials: true
 }))
 app.use(cookieParser())
